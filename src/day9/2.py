b43dff9a66daf0a7743a02473534071c700a9be4
Martin Ruderer <martin.ruderer@orbitlabs.de>
2023-12-09 21:12:05 +0100

    Day 9

---
+++ src/day9/2.py
@@ -0,0 +1,32 @@
+
+def parse(lines):
+    for line in lines:
+        yield [int(x) for x in line.split()]
+
+def diffsp(line):
+    if all([x == 0 for x in line]):
+        return []
+    res = [a-b for a, b in zip(line, line[1:])]
+    return [res] + diffsp(res)
+
+def diffs(line):
+    return list(reversed([line] + diffsp(line)))
+
+def solve(ds):
+    res = 0
+    for a, b in zip(ds, ds[1:]):
+        res = res + b[0]
+    return res
+
+input = open("sample.txt").readlines()
+input = open("input.txt").readlines()
+
+
+lines = list(parse(input))
+
+print(diffs(lines[0]))
+
+s = [solve(diffs(line)) for line in lines]
+print(s)
+
+print(sum(s))
